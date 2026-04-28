3e0a792a615242b61cb517d8c23f1e1d559624e7
djwirz <djwirz@gmail.com>
2026-04-28 11:42:44 -0500

    Fix the contrived problem

--- perf/src/App.jsx
+++ perf/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { marked } from "marked";
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 
 import MarkdownPreview from "./MarkdownPreview";
 import markdownContent from "./markdownContent";
@@ -17,8 +17,8 @@ export default function App() {
     return () => clearInterval(interval);
   }, []);
 
-  const options = { text, theme };
-  const render = (text) => marked.parse(text);
+  const render = useCallback((text) => marked.parse(text), []);
+  const options = useMemo(() => ({ text, theme }), [text, theme]);
 
   return (
     <div className="app">
